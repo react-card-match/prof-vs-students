e9ebcd1b42a4ffa5a2846ec52e7a2d114527cdaa
BeautifulSoup35 <als477049@gmail.com>
2026-05-14 12:20:40 +0900

    턴 학생이 버튼 클릭할때마다 1증가 상태창에 변수 입력

--- src/components/StatusBar/StatusBar.jsx
+++ src/components/StatusBar/StatusBar.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import * as s from "./styles"
 
-function StatusBar() {
-
+function StatusBar({status}) {
+    const {id, hp, tired} = status;
 
     return (
         <>
             <div css={s.statusBar}>
                 <div css={s.hpBackBar}>
-                    <div css={s.hp()} />
+                    <div css={s.hp(hp)} />
                 </div>
-                <div css={s.tiredBackBar}>
-                    <div css={s.tired()} />
-                </div>
+                {
+                    id === "teacher" &&
+                    (<div css={s.tiredBackBar}>
+                        <div css={s.tired(tired)} />
+                    </div>) 
+                }
+                
             </div>
         </>
     )
--- src/components/UserController/UserController.jsx
+++ src/components/UserController/UserController.jsx
@@ -1,12 +1,13 @@
 import * as s from "./styles"
 
-function UserController() {
+function UserController({num, setNum}) {
     const UserControll = ["질문한다", "수강생", "집중한다", "대답한다"];
+    
+
     return (
         <>
         <div css={s.buttons}>
-
-            {UserControll.map(a => <button css={s.button} >{a}</button>)}
+            {UserControll.map(a => <button onClick={()=> {setNum(num+1)}}key={a} css={s.button} >{a}</button>)}
         </div>
             
         </>
